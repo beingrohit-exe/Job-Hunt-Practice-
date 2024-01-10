66d29aa2a0ce6228037e4f248a40e8a29de9aa99
rohit <rohitparih@gmail.com>
2024-01-10 11:54:43 +0530

    Recursion -> `Keypad Combinations`

--- DSA/Recursion/KeypadCombination.java
+++ DSA/Recursion/KeypadCombination.java
@@ -6,22 +6,27 @@ public class KeypadCombination {
 
     static String[] codes = { ".;", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz" };
 
-    public static ArrayList<String> getCombinations(String str) {
-        char ch = str.charAt(0);
-        String ros = str.substring(1);
-        ArrayList<String> combinations = getCombinations(ros);
-        ArrayList<String> list = new ArrayList<>();
-        String codesForeach = codes[ch];
-        for (int i = 0; i < codesForeach.length(); i++) {
-            char chCode = codesForeach.charAt(i);
-            for (String co : combinations) {
-                list.add(chCode + co);
+    public static ArrayList<String> getCombinations(String data) {
+        if (data.isEmpty()) {
+            ArrayList<String> ar = new ArrayList<>();
+            ar.add("");
+            return ar;
+        }
+        char c = data.charAt(0);
+        String subString = data.substring(1);
+        ArrayList<String> combinations = getCombinations(subString);
+        ArrayList<String> re = new ArrayList<>();
+        String code = codes[c - '0'];
+        for (int i=0 ; i<code.length() ; i++) {
+            char c1 = code.charAt(i);
+            for (String a : combinations) {
+                re.add(c1 + a);
             }
         }
-        return list;
+        return re;
     }
 
     public static void main(String[] args) {
-
+        System.out.println(getCombinations("257"));
     }
 }
