ad2d2ec5a02b98b6ff7d9073f658d2609ac05c3a
rohit <rohitparih@gmail.com>
2024-01-11 12:33:11 +0530

    Recursion -> `Get Maze Paths`

---
+++ DSA/Recursion/GetMazePaths.java
@@ -0,0 +1,41 @@
+package DSA.Recursion;
+
+import java.util.ArrayList;
+
+public class GetMazePaths {
+
+    public static ArrayList<String> getMazePaths(int sr, int sc, int dr, int dc) {
+
+        if (sr == dr && sc == dc) {
+            ArrayList<String> bc = new ArrayList<>();
+            bc.add("");
+            return bc;
+        }
+        ArrayList<String> horizontalPaths = new ArrayList<>();
+        ArrayList<String> verticalPaths = new ArrayList<>();
+
+        if (sc < dc) {
+            horizontalPaths = getMazePaths(sr, sc + 1, dr, dc);
+        }
+
+        if (sr < dc) {
+            verticalPaths = getMazePaths(sr + 1, sc, dr, dc);
+        }
+
+        ArrayList<String> ar = new ArrayList<>();
+        for (String hPath : horizontalPaths) {
+            ar.add("h" + hPath);
+        }
+
+        for (String vPath : verticalPaths) {
+            ar.add("v" + vPath);
+        }
+
+        return ar;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<String> mazePaths = getMazePaths(1, 1, 4, 4);
+        System.out.println(mazePaths);
+    }
+}
