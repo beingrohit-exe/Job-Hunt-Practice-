17c6c25b88f363d28aca455251bd112dab53ecf7
rohit <rohitparih@gmail.com>
2024-01-15 12:57:09 +0530

    Generic Tree -> `Constructor`

---
+++ DSA/GenericTrees/TreeConstructor.java
@@ -0,0 +1,42 @@
+package DSA.GenericTrees;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Owner - Rohit Parihar
+ * Author - rohit
+ * Project - Job Hunt
+ * Package - DSA.GenericTrees
+ * Created_on - 15 January-2024
+ * Created_at - 12 : 35
+ */
+
+public class TreeConstructor {
+
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        int[] array = {10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1, -1};
+        Node root;
+        Stack<Node> st = new Stack<>();
+        for (int i=0 ; i<array.length ; i++) {
+            if (array[i] == -1) {
+                st.pop();
+            } else {
+                Node node = new Node();
+                node.data = array[i];
+
+                if (st.size() > 0) {
+                    st.peek().children.add(node);
+                } else {
+                    root = node;
+                }
+                st.push(node);
+            }
+        }
+    }
+}
